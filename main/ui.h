dc6dd1df4f09320cc8115857fa5be9cabc6ee235
JiuzZheyang <127168170+JiuzZheyang@users.noreply.github.com>
2026-07-13 01:30:32 +0800

    Fix set_monitor_param: 6 params (cpu_rate/temp/power + gpu_rate/temp/power)

--- main/ui.h
+++ main/ui.h
@@ -3,6 +3,7 @@
 
 void ui_init(void);
 void set_monitor_connect_state(bool connected);
-void set_monitor_param(int cpu_rate, int cpu_temp, int mem_rate, int mem_use);
+void set_monitor_param(int cpu_rate, int cpu_temp, int cpu_power,
+                       int gpu_rate, int gpu_temp, int gpu_power);
 
 #endif
