ede9b12a47d2f2c337079bb102053cacb061bc3b
JiuzZheyang <127168170+JiuzZheyang@users.noreply.github.com>
2026-07-13 01:36:31 +0800

    Add lv_conf.h to lvgl__lvgl (../../lv_conf.h from src/)

---
+++ managed_components/lvgl__lvgl/lv_conf.h
@@ -0,0 +1,27 @@
+/* lv_conf.h - LVGL v9 minimal config for T-Display-S3 */
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_CONF_PATH                 ""
+
+#define LV_COLOR_DEPTH               16
+#define LV_COLOR_16_SWAP             1
+#define LV_MEM_SIZE                  (32 * 1024)
+#define LV_MEM_CUSTOM                0
+#define LV_USE_OS                    LV_OS_FREERTOS
+#define LV_DISP_DEF_REFR_RATE       60
+#define LV_FONT_MONTSERRAT_14       1
+#define LV_FONT_MONTSERRAT_18       1
+#define LV_FONT_MONTSERRAT_24       1
+#define LV_USE_FILESYSTEM            0
+#define LV_USE_ANIMATION            1
+#define LV_USE_LABEL                1
+#define LV_USE_RECT                 1
+#define LV_USE_ARC                  1
+#define LV_USE_BAR                  1
+#define LV_USE_BTN                  1
+#define LV_USE_IMG                  1
+#define LV_USE_LINE                 1
+#define LV_USE_ROLLER               1
+
+#endif
