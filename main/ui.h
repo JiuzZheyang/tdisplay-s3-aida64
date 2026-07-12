971254de1cf8044b3e5915384f46d4ead16f297f
JiuzZheyang <127168170+JiuzZheyang@users.noreply.github.com>
2026-07-13 01:36:29 +0800

    ui.h: add #include <stdbool.h> for bool type

--- main/ui.h
+++ main/ui.h
@@ -1,6 +1,8 @@
 #ifndef UI_H
 #define UI_H
 
+#include <stdbool.h>
+
 void ui_init(void);
 void set_monitor_connect_state(bool connected);
 void set_monitor_param(int cpu_rate, int cpu_temp, int cpu_power,
